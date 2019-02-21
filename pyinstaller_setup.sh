213d73c45ce590e2a7dba97a983ba170b7966bd1
Cole Deisseroth <coledeisseroth@gmail.com>
2019-02-20 16:03:34 -0800

    added a pyinstaller setup script

---
+++ pyinstaller_setup.sh
@@ -0,0 +1,2 @@
+pyinstaller ClinPhenApp -p clinphen_src/ --add-data 'clinphen_src/data:clinphen_src/data/' --add-data 'clinphen_src/misc:clinphen_src/misc/' -F -i ClinPhen_Logo_small.ico -i ClinPhen_logo_small.icns -w
+
